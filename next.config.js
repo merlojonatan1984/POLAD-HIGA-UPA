a910f1c682d30e5d0998576a44d56d7f6cffb711
merlojonatan1984 <merlojonatan1984@gmail.com>
2026-05-25 17:32:32 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_LUGAR: process.env.NEXT_PUBLIC_LUGAR || 'HIGA',
+  },
 }
 
 module.exports = nextConfig
